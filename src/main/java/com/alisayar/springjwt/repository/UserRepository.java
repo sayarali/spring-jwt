964016845778a75919f8286beecbe3fdfd91c372
Ali Sayar <alisayar07@gmail.com>
2023-06-07 19:27:50 +0300

    UserRepository

---
+++ src/main/java/com/alisayar/springjwt/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.alisayar.springjwt.repository;
+
+import com.alisayar.springjwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
