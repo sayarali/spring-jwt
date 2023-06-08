47e097667bef1caa3a01f1cdea894284459ac3ec
Ali Sayar <alisayar07@gmail.com>
2023-06-08 19:35:23 +0300

    JwtService

--- src/main/java/com/alisayar/springjwt/service/JwtService.java
+++ src/main/java/com/alisayar/springjwt/service/JwtService.java
@@ -2,19 +2,25 @@ package com.alisayar.springjwt.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
 
-    @Value("${security.jwt.secret}")
-    private String SECRET_KEY;
+    //@Value("${security.jwt.secret}")
+    private String SECRET_KEY = "FDSwasdfewHTFGSBsdopfgklvtyfuvsdgSGDFVPSDFgsdfglvvsdWfDTUfgsTYhv";
 
     public String getUsernameFromJwt(String token) {
         return exportToken(token, Claims::getSubject);
@@ -35,5 +41,15 @@ public class JwtService {
     public boolean tokenControl(String token, UserDetails userDetails) {
         final String username = getUsernameFromJwt(token);
         return (username.equals(userDetails.getUsername()) && !exportToken(token, Claims::getExpiration).before(new Date()));
+    }
+
+    public String generateToken(UserDetails user) {
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 }
