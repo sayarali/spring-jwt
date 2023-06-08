38f2ed0b02aaf7d77a39b3515351afe12be46576
Ali Sayar <alisayar07@gmail.com>
2023-06-08 19:35:06 +0300

    UserRequestDto

---
+++ src/main/java/com/alisayar/springjwt/dto/UserRequestDto.java
@@ -0,0 +1,17 @@
+package com.alisayar.springjwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+
+    private String username;
+    String password;
+
+}
