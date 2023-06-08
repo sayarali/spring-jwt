d1b300894e21f685d44135276bb66129115ff096
Ali Sayar <alisayar07@gmail.com>
2023-06-08 19:34:55 +0300

    User

--- src/main/java/com/alisayar/springjwt/entity/User.java
+++ src/main/java/com/alisayar/springjwt/entity/User.java
@@ -21,6 +21,7 @@ import java.util.List;
 @Table(name = "users")
 public class User implements UserDetails{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
