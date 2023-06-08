ba2728969aafd4ac9d0a9f68e277ffd56f5ea4a0
Ali Sayar <alisayar07@gmail.com>
2023-06-08 19:36:02 +0300

    ApplicationConfig

--- src/main/java/com/alisayar/springjwt/config/ApplicationConfig.java
+++ src/main/java/com/alisayar/springjwt/config/ApplicationConfig.java
@@ -38,6 +38,7 @@ public class ApplicationConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
         throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
